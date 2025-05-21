9f654413a7aa15ceb5143793192808356688566a
Andrew Kippenhan <ajkipp@bellsouth.net>
2025-05-21 11:54:26 -0400

    init project

---
+++ src/main/java/dev/ajkipp/weather/DisneyWeatherInterviewApplication.java
@@ -0,0 +1,13 @@
+package dev.ajkipp.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyWeatherInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisneyWeatherInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ajkipp/weather/DisneyWeatherInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ajkipp.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyWeatherInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
